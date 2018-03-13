f27240abe2a96be3c46cef2ef90de005f1ef2228
Ndumbu <k1630472@nmscde004055.nms.kcl.ac.uk>
2018-03-13 15:44:50 +0000

    Added template of method to localize on Map

--- Localization.java
+++ Localization.java
@@ -5,17 +5,17 @@ public Localization {
 	public static final float SENSOR_FAIL = 0.8;
 	public static final float SENSOR_HIT = 0.2;
 	public static final float UNDERSHOOT = 0.1;
-	public static final float FORWARD = 0.9; 
-	
+	public static final float FORWARD = 0.9;
+
 	//1 dimentional representation of the line the robot is travelling on.
-	private static final Boolean[] MAP = {true, true, false, false, false, true, false, false, false, true, false, false, false, true, true, true, true, true, true, true, true, true};	
+	private static final Boolean[] LINE= {true, true, false, false, false, true, false, false, false, true, false, false, false, true, true, true, true, true, true, true, true, true};
 
 	//array to hold probabilities of each position.
-	private float[MAP.length-1] Pr;
+	private float[LINE.length-1] Pr;
 
 	public Localization(){
 		for(float f : Pr){
-			f = 1/MAP.length;
+			f = 1/LINE.length;
 		}
 	}
 
@@ -25,16 +25,16 @@ public Localization {
 	*	@param colour	reading from colour sensor
 	*	@param move		boolean to see if robot is moving
 	*/
-	public float[] sensorFilter(float colour, Boolean move){
+	public float[] sensorFilter(float colour){
 		//constant for normalization of probabilities
 		int n = 0;
 
 
 		//calculating probabilities in Pr after a sensor reading
-		for(int i = 0; i < MAP.length; i++){
-			
+		for(int i = 0; i < LINE.length; i++){
+
 			//if colour sensor reading matches the map reading
-			if(((colour == Color.BLUE) && MAP[i]) || !((colour == Color.BLUE) && !MAP[i])){
+			if(((colour == Color.BLUE) && LINE[i]) || !((colour == Color.BLUE) && !LINE[i])){
 				Pr[i] = Pr[i] * SENSOR_HIT;
 				n += Pr[i];
 			} else {
@@ -42,15 +42,18 @@ public Localization {
 				n += Pr[i];
 			}
 
-		}	
+		}
 
 		//normalizing probabilities
 		for(float f : Pr){
 			f /= n;
 		}
 
-		//calculating probabilities in Pr after an action 
-		for(int i = 0; i < MAP.length; i++){
+		return Pr;
+	}
+
+	public float[] motionUpdate(){
+		for(int i = 0; i < LINE.length; i++){
 			if(i == 0){
 				Pr[i] *= UNDERSHOOT;
 			} else {
@@ -59,6 +62,39 @@ public Localization {
 		}
 
 		return Pr;
+	}
+
+	public Pair<int,int> robotLocation(float[] p){
+		float max;
+		int n;
+
+		for(float f : p){
+			float m;
+
+			if(f>m){
+				m = f;
+				max = m;
+			} else if(f == m && m > 0){
+				n++;
+			}
+
+		}
+
+		if(n>1){
+			n = 0;
+			return null;
+		} else {
+			n = 0;
+
+			for(;n<p.length;n++){
+				if(max>0.2){
+					//implement method to transpose location on line to location on main map
+				}
+				return null;
+			}
+
+		}
+
 	}
 
 }
