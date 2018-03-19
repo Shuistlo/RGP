11397413e2a472da71c2c79d7eabdc60ffc5d23b
Jeffrey Ndumbu <jeffrey.ndumbu@kcl.ac.uk>
2018-03-19 16:31:06 +0000

    Localisation with current AStarCell setup (as seperate class) and 30x30 map

--- Localization.java
+++ Localization.java
@@ -1,50 +1,45 @@
-package Localization;
-
-import lejos.robotics.Color;
-
-public class Localization {
+public class Localisation {
 
 	public static final double SENSOR_FAIL = 0.8;
 	public static final double SENSOR_HIT = 0.2;
 	public static final double UNDERSHOOT = 0.1;
 	public static final double FORWARD = 0.9;
+	public static final double THRESHOLD = 0.1;
 
 	//1 Dimensional representation of the line the robot is travelling on.
-	private static final Boolean[] LINE= {true,true,false,false,true,true,true,false,false,true,true,true,false,true,true,true,false,false,false,true,true,true};
+	private final Boolean[] LINE= {true,true,false,false,true,true,true,false,false,true,true,true,false,true,true,true,false,false,false,true,true,true};
 
 	//array to hold probabilities of each position.
-	private double[] Pr;
+	private double[] Pr = new double[LINE.length];
 
-	public Localization(){
-		Pr = new double[LINE.length -1];
-		for(double d : Pr){
-			d = 1/LINE.length;
+	public Localisation(){
+		for(int i = 0; i<LINE.length;i++){
+			Pr[i] = (double)1/LINE.length;
 		}
 	}
 
-	/** Uses Markov Localization to estimate the most likely position of the robot,
-	*	using a pre-established map.
+	/** Uses Markov Localisation to estimate the most likely position of the robot
+	*	using a map.
 	*
 	*	@param colour	reading from colour sensor
-	*	@param move		boolean to see if robot is moving
 	*/
-	public double[] sensorFilter(float colour){
+
+	public void filter(float colour){
+
 		//constant for normalisation of probabilities
 		int n = 0;
 
-　
 		//calculating probabilities in Pr after a sensor reading
-		for(int i = 0; i < LINE.length; i++){
+		for(int i = 0; i < Pr.length; i++){
 
 			//if colour sensor reading matches the map reading
-			if(((colour < 0.1 && LINE[i]) || !((colour < 0.1) && !LINE[i]))){
-				Pr[i] = Pr[i] * SENSOR_HIT;
+			if(colour < THRESHOLD && LINE[i]){
+				Pr[i] *= SENSOR_HIT;
 				n += Pr[i];
-			} else {
-				Pr[i] = Pr[i] * SENSOR_FAIL;
+			} else if(colour > THRESHOLD && LINE[i]){
+				Pr[i] *= SENSOR_FAIL;
 				n += Pr[i];
 			}
-
 		}
 
 		//Normalising probabilities
@@ -52,10 +47,7 @@ public class Localization {
 			d = d/n;
 		}
 
-		return Pr;
-	}
-
-	public double[] motionUpdate(){
+		//motion update
 		for(int i = 0; i < LINE.length; i++){
 			if(i == 0){
 				Pr[i] *= UNDERSHOOT;
@@ -64,40 +56,47 @@ public class Localization {
 			}
 		}
 
-		return Pr;
+	}
+	
+	public void printProbArray(){
+		for(double d : Pr){
+			System.out.print(d+", ");
+		}
 	}
 
-	public Cell mapLocation(double[] p){
-		double max;
-		int n;
+	public Cell mapLocation(){
 
-		for(double d : p){
-			double m;
+		double max = 0;
+		int n = 0;
 
-			if(d>m){
-				m = d;
-				max = m;
-			} else if(d == m && m > 0){
+		for(double d : Pr){
+			if(d>max){
+				max = d;
+			} else if(d == max){
 				n++;
 			}
-
 		}
 
-		if(n>1){
-			n = 0;
-			return null;
-		} else {
-			n = 0;
-
-			for(;n<p.length;n++){
-				if(max>0.2){
-					//complete line-map integration
+		if(n==1){
+			for(n = 0; n<Pr.length; n++){
+				if(Pr[n] == max && max > 0.10f){
+					if(n>28){
+						return new AStarCell(7,6);
+					} else if(n>22){
+						return new AStarCell(8,5);
+					} else if(n>16){
+						return new AStarCell(9,4);
+					} else if(n>10){
+						return new AStarCell(10,3);
+					} else if(n>4){
+						return new AStarCell(11,2);
+					}
 				}
-				return null;
 			}
+		} 
 
-		}
-
+		n = 0;
+		return null;
 	}
 
 }
